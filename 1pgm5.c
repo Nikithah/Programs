8b6bf9bb51099b62484e4c8c0894e85993b8e8fa
Nikithah <nikithahebbar02@gmail.com>
2021-10-07 20:03:34 +0530

    pgm5

---
+++ 1pgm5.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+void main() 
+{
+    int num=22; 
+    printf("Working of left shift operator\n"); 
+    printf("22<<1 = %d\n22<<2 = %d\n22<<3 = %d\n",22<<1,22<<2,22<<3); 
+    printf("Working of right shift operator\n"); 
+    printf("22>>1 = %d\n22>>2 = %d\n22>>3 = %d",22>>1,22>>2,22>>3); 
+}
